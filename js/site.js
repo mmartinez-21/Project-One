0f58b95c1900c2f7caac07d7d297b954196d022e
mmartinez-21 <60053322+mmartinez-21@users.noreply.github.com>
2021-03-16 22:01:53 -0500

    Update js file

--- js/site.js
+++ js/site.js
@@ -3,3 +3,17 @@ jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 });
+
+function incompSub(){
+  var inName = document.getElementById("nameInput").value;
+  var inEmail = document.getElementById("emailInput").value;
+  var inDoB = document.getElementById("DoB").value;
+  var inCB = document.getElementById("cbInput").value;
+  
+  if ((inCB == "") || (inEmail == "") || (inName == "") || (inDoB == "")){
+    alert("Fill out all the required info to receive Mars Year Round Trips!");
+  }
+  else {
+    alert("Thank you for your membership " + inName + "! We look forward to your travels!");
+  }
+}
